017244d0b4ba00d4757d55bdfa29924bfcea6ae6
DeBoiCruzyy <cruz.arreola.heat@gmail.com>
2025-10-13 12:36:05 -0700

    Initial commit for settings page

--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -4,3 +4,16 @@
 </svelte:head>
 
 <h1>Settings</h1>
+
+<div>
+    <form action="">
+        <label for="username">Username:</label><br>
+        <input id="username" type="text">
+        <label for="password">Password:</label><br>
+        <input id="password" type="text">
+        <label for="email">Email:</label><br>
+        <input id="email" type="email">
+        <input id="submit" type="submit">
+    </form>
+</div>
+
