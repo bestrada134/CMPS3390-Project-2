290f1086e864e8aea932317755c40dfbd5c61ab4
Bryan Estrada <bestrada13@csub.edu>
2025-10-15 02:35:08 -0700

    working buttons on dashboard and edits in app.css

--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -7,8 +7,30 @@
     <title>Dashboard | Library Tracker</title>
 </svelte:head>
 
-<h1 class="text-2xl font-bold mb-3">Dashboard</h1>
-<p>
-    Welcome, {data.user.Username}! Use the navbar above to visit Books and
-    Settings.
-</p>
+
+<h1 class="text-4xl font-bold mb-3">Dashboard</h1>
+<h1 class="font-bold text-3xl mb-8">
+    Welcome, {data.user.Username}!
+</h1>
+
+
+<div class="grid grid-cols-3 md:grid-cols-3 gap-4">
+    <div>
+        <a href="#" class="card-dashboard">
+        <h5>Loans</h5>
+        <p>Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </a>
+    </div>
+    <div>
+        <a href="#" class="card-dashboard">
+        <h5>Library</h5>
+        <p>Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </a>
+    </div>
+    <div>
+        <a href="#" class="card-dashboard">
+        <h5>Settings</h5>
+        <p>Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </a>
+    </div>
+</div>
