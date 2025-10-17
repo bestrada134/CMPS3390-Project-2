b4aef5d69e4d7780cc881592ab84feb0cbf45c09
DeBoiCruzyy <cruz.arreola.heat@gmail.com>
2025-10-17 12:43:43 -0700

    Changed settings page logic

--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -11,17 +11,16 @@
 <h1>Settings</h1>
 
 <div>
-    <form action="?/update" method="post">
-        <label for="name">Name:</label><br>
-        <input type="text" name="name" id="name" value="{data?.user?.Username ?? ''}" /><br>
-
-        <label for="email">Email:</label><br>
-        <input type="email" name="email" id="email" value="{data?.user?.Email ?? ''}" /><br>
-
-        <label for="password">Password:</label><br>
-        <input type="text" name="password" id="password"><br><br>
-
+    <form class="max-w-sm mx-auto">
+        <label for="options" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Choose what to change</label>
+        <select id="options" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 darl:placeholder-gray-400 dark:texxt-white dark:focus:ring-blue-500  dark:focus:border-blue-500">
+            <option value="Username">Username</option>
+            <option value="Email">Email</option>
+            <option value="Password">Password</option>
+        </select>
+        <input type="text" name="newValue" placeholder="New Value">
         <button type="submit">Save</button>
+
     </form>
 
     <style>
@@ -38,13 +37,13 @@
             border-radius: 8px;
             font: inherit;
         }
-        form input: focus {
+        form input:focus {
             outline: none;
             border-color: #6f9cf6;
             box-shadow: 0 0 0 2px rgba(111,156,246,.2);
         }
         form button[type="submit"]{
-            margin top: .25rem;
+            margin-top: .25rem;
             padding: .55rem .9rem;
             border: 0;
             border-radius: 8px;
